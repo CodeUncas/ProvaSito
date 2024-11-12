b12d77dff3ba134b30b2d15313eb0cc8c36839a7
Uncas <74324570+CodeUncas@users.noreply.github.com>
2024-11-12 22:53:25 +0100

    Update googlesheetScript.py

--- .github/workflows/googlesheetScript.py
+++ .github/workflows/googlesheetScript.py
@@ -3,7 +3,7 @@ from googleapiclient.discovery import build
 
 def build_sheets_service():
     # Percorso del file delle credenziali
-    credentials_path = '~/credentials.json'
+    credentials_path = '../../credentials.json'
     # Carica le credenziali direttamente dal file
     credentials = service_account.Credentials.from_service_account_file(
         filename=credentials_path
